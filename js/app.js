b8bb665de4a207939dede52fdd5f110b273e5bdf
ali <alihasansaad1@gmail.com>
2025-06-29 10:44:43 +0300

    Cache buttoms amd variables

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*--------------constants--------------*/
+const choices=['rock','paper','scissors']
+
+/*--------------variables--------------*/
+let msg
+let playerChoice
+let computerChoice
+
+/*--------------cached elements referneces--------------*/
+const rockButtonElement = document.querySelector("#rock")
+console.log(rockButtonElement)
+const paperButtonElement = document.querySelector("#paper")
+console.log(paperButtonElement)
+const scissorsButtonElement=document.querySelector("#scissors")
+console.log(scissorsButtonElement)
+/*--------------functions--------------*/
+
+/*--------------constants--------------*/
+
+/*--------------constants--------------*/
