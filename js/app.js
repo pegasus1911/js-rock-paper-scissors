4b5d2a868822f9284ea120f4d56e388b2d0d34d7
ali <alihasansaad1@gmail.com>
2025-06-29 13:48:41 +0300

    message describing code changes

--- js/app.js
+++ js/app.js
@@ -5,38 +5,83 @@ const choices=['rock','paper','scissors']
 let msg
 let playerChoice
 let computerChoice
+let playerResult    
+let computerResul
 
 /*--------------cached elements referneces--------------*/
 const rockButtonElement = document.querySelector("#rock")
-console.log(rockButtonElement)
+// console.log(rockButtonElement)
 const paperButtonElement = document.querySelector("#paper")
-console.log(paperButtonElement)
+// console.log(paperButtonElement)
 const scissorsButtonElement=document.querySelector("#scissors")
-console.log(scissorsButtonElement)
+// console.log(scissorsButtonElement)
+const resultDisplayElement = document.querySelector("#result-display")
+// console.log(resultDisplayElement)
+const resetButtonElemenet=document.querySelector("#resetButton")
+console.log(resetButtonElemenet)
 /*--------------functions--------------*/
 
 //initialize game state
-function play(){
+function play(event){
     //
-    getComputerChoice()
-    console.log('computer choice is = '+computerChoice)
+    computerChoice=getComputerChoice()
+    playerChoice=event.target.id;
+    compare()
     render()
+    
 }
 function getComputerChoice(){
     //generate a random number 0-2
     const randomIndex= Math.floor(Math.random()*choices.length)
+    
     // select the item from the array
-    computerChoice= choices[randomIndex]
+    return choices[randomIndex]
 }
 
 getComputerChoice()
 
-play()
+// play()
 
 function render(){
-
+    resultDisplayElement.textContent="Computer chose = " + computerChoice +" and you chose "+ playerChoice+" "+msg
 }
 
-/*--------------constants--------------*/
+function compare(){
+    if(playerChoice==="rock"&&computerChoice==="paper"){
+         msg="Computer Won"
+    }
+    else if(playerChoice==='rock'&&computerChoice==='scissors'){
+        msg='You won'
+    }
+    else if(playerChoice==='rock'&&computerChoice==='rock'){
+        msg='DRAW'
+    }
+    else if(playerChoice==='paper'&&computerChoice==='rock'){
+        msg="you won"
+    }
+    else if(playerChoice==='paper'&&computerChoice==='scissors'){
+        msg='computer won'
+    }
+    else if(playerChoice==='paper'&&computerChoice==='paper'){
+        msg='DRAW'
+    }
+    else if(playerChoice==="scissors"&&computerChoice==='paper'){
+        msg='player won'
+    }
+    else if(playerChoice==="scissors"&&computerChoice==='rock'){
+        msg='comuter won'
+    }
+    else{
+        msg='Draw'
+    }
 
+}
+function reset(){
+    resultDisplayElement.textContent='Game reseted, play choose a choice'
+}
+/*--------------Event listinetr--------------*/
+rockButtonElement.addEventListener('click',play)
+paperButtonElement.addEventListener('click',play)
+scissorsButtonElement.addEventListener('click',play)
+resetButtonElemenet.addEventListener('click',reset)
 /*--------------constants--------------*/
